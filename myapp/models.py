5b12e2a011920b970b35db8cf6de9709581f5855
AnkurMangroliya <ankurmangroliya099@gmail.com>
2024-07-09 18:57:20 -0400

    Updated models.py while adding Review and WIshlist class.

--- myapp/models.py
+++ myapp/models.py
@@ -1,20 +1,3 @@
 from django.db import models
 
-from django.contrib.auth.models import User
 # Create your models here.
-
-
-class Review(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.PositiveIntegerField()
-    comment = models.TextField()
-    date_added = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f'Review by {self.user.username} for {self.product.name}'
-
-class Wishlist(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product)
-    date_added = models.DateTimeField(auto_now_add=True)
--- products/models.py
+++ products/models.py
@@ -21,3 +21,19 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Review by {self.user.username} for {self.product.name}'
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    date_added = models.DateTimeField(auto_now_add=True)
