6989dcbbeaf43e6fdfa38be4f564437dda65a76f
harshmm21 <moradi21@uwindsor.ca>
2024-07-14 12:39:08 -0400

    Added the password reset feature with email-based verification and token-based authentication.

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,11 +3,19 @@ from .views import register
 from .views import login
 from .views import logout
 from .views import dashboard
+from .views import activate
+from .views import resetpassword
+from .views import resetpassword_validate
+from .views import forgotpassword
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
     path('dashboard/', dashboard, name='dashboard'),
-    path('', dashboard, name='dashboard')
+    path('', dashboard, name='dashboard'),
+    path('activate/<uidb64>/<token>/', activate, name='activate'),
+    path('resetpassword_validate/<uidb64>/<token>/', resetpassword_validate, name='resetpassword_validate'),
+    path('forgotpassword/', forgotpassword, name='forgotpassword'),
+    path('resetpassword', resetpassword, name='resetpassword'),
 ]
