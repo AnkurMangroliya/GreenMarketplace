a0d0942f5e17567baaf64180b589796bbc69ec00
MohilJivani <mohiljivani1911@gmail.com>
2024-07-16 10:24:06 -0400

    orders added

--- GreenMarketplace/urls.py
+++ GreenMarketplace/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from accounts import urls as accounts_urls
+from orders import urls as orders_urls
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include(accounts_urls)),
-    path('', views.home, name='home')
-
+    path('', views.home, name='home'),
+    path('orders/', include(orders_urls))
 ]
