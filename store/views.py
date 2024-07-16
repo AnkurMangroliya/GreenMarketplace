fd4235ca683ce86137d694b1b2089c66fa3c4164
AnkurMangroliya <ankurmangroliya099@gmail.com>
2024-07-16 09:09:03 -0400

    I have updated the views.py while adding new function for store.

--- store/views.py
+++ store/views.py
@@ -1,7 +1,25 @@
 from .models import Product
-
-
+from django.shortcuts import render, get_object_or_404, redirect
+from category.models import Category
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def store(request, category_slug=None):
     categories = None
     products = None
+    if category_slug is not None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 1)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
+        product_count = products.count()
+    else:
+        # mostra apenas produtos disponiveis e os ordena pelo id
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
+        product_count = products.count()
+    context = {'products': paged_products,
+               'product_count': product_count}
+    return render(request, 'store/store.html', context)
